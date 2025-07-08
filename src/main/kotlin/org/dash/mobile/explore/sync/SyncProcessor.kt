3778baaceba2510bf0cc0e9acce8fb62986cae8f
HashEngineering <hashengineeringsolutions@gmail.com>
2025-07-07 19:03:15 -0700

    chore: update to v4 database file only

--- src/main/kotlin/org/dash/mobile/explore/sync/SyncProcessor.kt
+++ src/main/kotlin/org/dash/mobile/explore/sync/SyncProcessor.kt
@@ -64,9 +64,9 @@ class SyncProcessor(private val mode: OperationMode) {
             logger.debug("DB file checksum $dbFileChecksum")
 
             val dbZipFileName = when (mode) {
-                OperationMode.PRODUCTION -> "${dbFile.nameWithoutExtension}-v3.zip"
-                OperationMode.TESTNET -> "${dbFile.nameWithoutExtension}-v3-testnet.zip"
-                OperationMode.DEVNET -> "${dbFile.nameWithoutExtension}-v3-devnet.zip"
+                OperationMode.PRODUCTION -> "${dbFile.nameWithoutExtension}-v4.zip"
+                OperationMode.TESTNET -> "${dbFile.nameWithoutExtension}-v4-testnet.zip"
+                OperationMode.DEVNET -> "${dbFile.nameWithoutExtension}-v4-devnet.zip"
             }
 
             val dbZipFile = File(workingDir, dbZipFileName)
